23749f35ab975b7b4cb1674836147797b71c5e39
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2026-04-11 23:04:53 +0200

    add: chapter-02-ex04-bean-naming

---
+++ chapter-02/ex04-bean-naming/src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context =
+                new AnnotationConfigApplicationContext(
+                        ProjectConfig.class);
+
+        //Parrot p = context.getBean(Parrot.class);
+        //System.out.println(p.getName());
+        //Exception in thread "main" org.springframework.beans.factory.NoUniqueBeanDefinitionException:
+        // No qualifying bean of type 'main.Parrot' available: expected single matching bean
+        // but found 3: parrot1,parrot2,parrot3
+
+        Parrot p = context.getBean("miki", Parrot.class);
+        System.out.println(p.getName());
+
+
+    }
+}
