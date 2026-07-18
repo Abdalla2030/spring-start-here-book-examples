403d47d94f8ce51ea143b5921ba017417c2abb7f
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2026-07-19 01:09:08 +0300

    add: chapter-03-ex02-direct-wiring

---
+++ chapter-03/ex02-direct-wiring/src/main/java/main/Parrot.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Parrot {
+
+    public Parrot() {
+        System.out.println("Parrot created");
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot : " + name;
+    }
+}
